5b4f787c0a2286713c684e973c6cbf0cd7a5f058
TH-montassar <montassar.themri@gmail.com>
2026-03-07 09:00:23 +0100

    conf docker

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "📦 Installing dependencies..."
+npm install
+
+echo "⚙️ Generating Prisma client..."
+npx prisma generate
+
+echo "🗄️ Running migrations..."
+npx prisma migrate deploy
+
+echo "🚀 Starting dev server..."
+exec npm run dev
