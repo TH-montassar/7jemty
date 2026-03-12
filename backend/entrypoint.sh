989360f8c5e76e72a23d8cc297e8f39bd2b6599f
TH-montassar <montassar.themri@gmail.com>
2026-03-12 18:26:02 +0100

    fix:

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,10 +8,5 @@ npx prisma generate
 echo "🗄️ Running migrations..."
 npx prisma migrate deploy
 
-if [ "$NODE_ENV" = "production" ]; then
-    echo "🚀 Starting PRODUCTION server..."
-    exec npm run start:prod
-else
-    echo "🚀 Starting DEV server..."
-    exec npm run dev
-fi
+echo "🚀 Starting PRODUCTION server..."
+exec npm run start:prod
